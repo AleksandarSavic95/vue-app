027e8f09be079ed2d967e5d9108cf7538ec6f256
AleksandarSavic95 <alek_bn@hotmail.com>
2018-04-27 14:51:02 +0200

    #ALSA-14 add login/register links to header and modify css style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,6 +8,14 @@
         <router-link to="/create" exact>Create a TODO</router-link>
       </li>
     </ul>
+    <ul id="auth">
+      <li>
+        <router-link to="/login" exact>Login</router-link>
+      </li>
+      <li>
+        <router-link to="/register" exact>Register</router-link>
+      </li>
+    </ul>
   </nav>
 </template>
 
@@ -15,29 +23,39 @@
 export default {}
 </script>
 
-<style scoped>
-ul{
+<style>
+nav {
+    background: #444;
+    padding: 14px 0;
+    height: 60px;
+}
+nav a {
+  color: #fff;
+  padding: 6px 8px;
+  border-radius: 10px;
+}
+nav a:hover {
+  color: rgb(216, 216, 216);
+}
+nav .router-link-active {
+    background: #eee;
+    color: #444;
+}
+nav .router-link-active:hover {
+    background: #eee;
+    color: #5c5c5c;
+}
+nav > ul {
     list-style-type: none;
     text-align: center;
-    margin: 0;
+    float: left;
 }
-li{
+nav > ul > li {
     display: inline-block;
     margin: 0 10px;
 }
-a{
-    color: #fff;
-    text-decoration: none;
-    padding: 6px 8px;
-    border-radius: 10px;
-}
-nav{
-    background: #444;
-    padding: 14px 0;
-    margin-bottom: 40px;
-}
-.router-link-active{
-    background: #eee;
-    color: #444;
+
+#auth {
+  float: right;
 }
 </style>
