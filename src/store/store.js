6830788dec2d8f4a7410b1b7777f2d8e7251f6cc
AleksandarSavic95 <alek_bn@hotmail.com>
2018-05-07 12:44:13 +0200

    #ALSA-29 get TodoItem from server, update it in vuex store (refresh bug)
    On `ViewItem` page refresh,getter can't find the requested item in store
    `console.log`s show it in `items`, but it won't get returned from getter

--- src/store/store.js
+++ src/store/store.js
@@ -14,7 +14,15 @@ export const store = new Vuex.Store({
   getters: {
     [constants.IS_LOGGED_IN]: state => !!state.token,
     [constants.STATUS]: state => state.status,
-    [constants.ITEMS]: state => state.items
+    [constants.GET_ITEMS]: state => state.items,
+    [constants.GET_ITEM]: state => (id) => {
+      console.log('getter GET_ITEM says:\n items: ')
+      console.log(state.items)
+      var found = state.items.find(item => item.id === id)
+      console.log('found, id')
+      console.log(found, id)
+      return found
+    }
   },
   mutations: {
     [constants.START_REQUEST] (state) {
@@ -31,23 +39,51 @@ export const store = new Vuex.Store({
     [constants.AUTH_ERROR] (state) {
       state.status = 'error'
     },
-    [constants.ITEMS] (state, items) {
+    [constants.GET_ITEMS] (state, items) {
       state.items = items
       state.status = ''
+    },
+    [constants.UPDATE_ITEM] (state, item) {
+      var i = state.items.findIndex(o => o.id === item.id)
+      console.log('looking for item ')
+      console.log(item)
+      console.log('in items:')
+      console.log(state.items)
+      console.log('index: ' + i + ' bool(state.items[i]) ' + !!state.items[i])
+      if (state.items[i]) {
+        state.items[i] = item
+      } else { // if `items` wasn't filled
+        state.items.push(item)
+      }
+      state.status = ''
+      console.log('update_item done!. items:')
+      console.log(state.items)
     }
   },
   actions: {
-    [constants.ITEMS]: ({state, commit}) => {
+    [constants.GET_ITEMS]: ({state, commit}) => {
       commit(constants.START_REQUEST) // show spinner
 
       return apiService.getItems()
         .then(resp => {
           const items = resp.data
-          console.log(items)
-          commit(constants.ITEMS, items)
+          commit(constants.GET_ITEMS, items)
         })
         .catch(err => {
           commit(constants.AUTH_ERROR, err)
+        })
+    },
+    [constants.GET_ITEM]: ({state, commit}, id) => {
+      commit(constants.START_REQUEST) // show spinner
+
+      return apiService.getItem(id)
+        .then(resp => {
+          const item = resp.data
+          console.log(item)
+          commit(constants.UPDATE_ITEM, item)
+        })
+        .catch(err => {
+          console.log('error ', err)
         })
     },
     register () {
